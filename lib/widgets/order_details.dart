2188e5afed4ad9395bb619404e9ab5a3971f6d78
j2 infotech <j2info1994@gmail.com>
2025-04-25 20:20:55 +0530

    commited by antony on 23-4-2025

--- lib/widgets/order_details.dart
+++ lib/widgets/order_details.dart
@@ -162,204 +162,247 @@ class _OrderItemDetailsScreenState extends State<OrderItemDetailsScreen> {
   Widget create_appropriateStepper()
   {
     Widget w=Container();
-    if(_statusValue==0 || _statusValue==1 || _statusValue==2 || _statusValue==3)
+
+    if(orderDetailsData.paymentDetails?.paymentStatus?.compareTo("2")==0  )
       {
-        w= (orderDetailsData.cartReturnRequests!=null&&orderDetailsData.cartReturnRequests!.id.toString().compareTo("0")!=0) ? Stepper(
-          controlsBuilder: (context,controller){
-            return const SizedBox.shrink();
-          },
-          currentStep: _statusValue,
-          steps: [
-            Step(
-              title: Text("Order Created"),
-              subtitle: Text(
-                  "You have created an order. \nDate :" +
-                      ((orderDetailsData!.cartOrder!.createdAt != null && orderDetailsData!.cartOrder!.createdAt!.isNotEmpty)
-                          ? getFormatedDate(orderDetailsData!.cartOrder!.createdAt!)
-                          : " No date available")
-              ),
-              content: SizedBox.shrink(),
-              isActive: _statusValue >= 0,
-            ),
-
-            Step(
-              title: Text("Packing"),
-              subtitle: Text("Order Packing is on progress.\nDate : "+((orderDetailsData!.cartOrder!.packedAt != null && orderDetailsData!.cartOrder!.packedAt!.isNotEmpty)
-                  ? getFormatedDate(orderDetailsData!.cartOrder!.packedAt!)
-                  : " No date available")),
-              content: SizedBox.shrink(),
-              isActive: _statusValue >= 1,
-            ),
-            Step(
-              title: Text("Out for delivery"),
-              subtitle: Text("Out for delivery.You will get your product soon.\nDate : "+((orderDetailsData!.cartOrder!.shippedAt != null && orderDetailsData!.cartOrder!.shippedAt!.isNotEmpty)
-                  ? getFormatedDate(orderDetailsData!.cartOrder!.shippedAt!)
-                  : " No date available")),
-              content: SizedBox.shrink(),
-              isActive: _statusValue >= 2,
-            ),
-
-            Step(
-              title: Text("Delivered"),
-              subtitle: Text("Order Delivered successfully.\nDate : "+((orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency != null && orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency!.isNotEmpty)
-                  ? getFormatedDate(orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency!)
-                  : " No date available")),
-              content: SizedBox.shrink(),
-              isActive: _statusValue >= 3,
-            ),
-
-
-            Step(
-              title:(orderDetailsData!.cartReturnRequests!.status.toString().compareTo("1")==0)?
-              Text("Return request accepted") :
-              (orderDetailsData!.cartReturnRequests!.status.toString().compareTo("2")==0 ?
-              Text("Return request Rejected") :
-              Text("Return request submitted on "+getFormatedDate(orderDetailsData!.cartReturnRequests!.createdAt.toString()))) ,
-              subtitle: (orderDetailsData!.cartReturnRequests!.status.toString().compareTo("1")==0)?
-  Padding(padding: EdgeInsets.all(ResponsiveInfo.isMobile(context)?5:10),
-
-                child:Text("Your return request accepted" + (orderDetailsData!.cartReturnRequests!.refundStatus.toString().compareTo("1")==0 ? "\n Refunded on "+((orderDetailsData!.cartReturnRequests!.refundedDate!=null)? getFormatedDate(orderDetailsData!.cartReturnRequests!.refundedDate.toString()) : "")  : "\nNot Refunded")
-
-
-                  ,style: TextStyle(color: Colors.green),) ,
-
-              ) : Container(),
-              content: SizedBox.shrink(),
-              isActive: _statusValue >= 3,
-            ),
-
-
-          ],
-        )  :Stepper(
-          controlsBuilder: (context,controller){
-            return const SizedBox.shrink();
-          },
-          currentStep: _statusValue,
-          steps: [
-            Step(
-              title: Text("Order Created"),
-              subtitle: Text(
-                  "You have created an order. \nDate :" +
-                      ((orderDetailsData!.cartOrder!.createdAt != null && orderDetailsData!.cartOrder!.createdAt!.isNotEmpty)
-                          ? getFormatedDate(orderDetailsData!.cartOrder!.createdAt!)
-                          : " No date available")
-              ),
-              content: SizedBox.shrink(),
-              isActive: _statusValue >= 0,
-            ),
-
-            Step(
-              title: Text("Packing"),
-              subtitle: Text("Order Packing is on progress.\nDate : "+((orderDetailsData!.cartOrder!.packedAt != null && orderDetailsData!.cartOrder!.packedAt!.isNotEmpty)
-                  ? getFormatedDate(orderDetailsData!.cartOrder!.packedAt!)
-                  : " No date available")),
-              content: SizedBox.shrink(),
-              isActive: _statusValue >= 1,
-            ),
-            Step(
-              title: Text("Out for delivery"),
-              subtitle: Text("Out for delivery.You will get your product soon.\nDate : "+((orderDetailsData!.cartOrder!.shippedAt != null && orderDetailsData!.cartOrder!.shippedAt!.isNotEmpty)
-                  ? getFormatedDate(orderDetailsData!.cartOrder!.shippedAt!)
-                  : " No date available")),
-              content: SizedBox.shrink(),
-              isActive: _statusValue >= 2,
-            ),
-
-            Step(
-              title: Text("Delivered"),
-              subtitle: Text("Order Delivered successfully.\nDate : "+((orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency != null && orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency!.isNotEmpty)
-                  ? getFormatedDate(orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency!)
-                  : " No date available")),
-              content: SizedBox.shrink(),
-              isActive: _statusValue >= 3,
-            ),
-
-
-          ],
-        );
-      }
-    else if(_statusValue==4){
-      w= Stepper(
-        controlsBuilder: (context,controller){
+        w=Stepper(steps: [
+          Step(
+            title: Text("Order Initiated"),
+            subtitle: Text("You have initiated to create an order"),
+            content: SizedBox.shrink(),
+            isActive: true,
+          ),
+          Step(
+            title: Text("Payment Not Completed"),
+            subtitle: Text("You have not completed your payment",style: TextStyle(color: Colors.redAccent),),
+            content: SizedBox.shrink(),
+            isActive: true,
+          )
+        ], controlsBuilder: (context,controller){
           return const SizedBox.shrink();
-        },
-        currentStep: _statusValue,
-        steps: [
-          Step(
-            title: Text("Order Created"),
-            subtitle: Text(
-                "You have created an order. \nDate :" +
-                    ((orderDetailsData!.cartOrder!.createdAt != null && orderDetailsData!.cartOrder!.createdAt!.isNotEmpty)
-                        ? getFormatedDate(orderDetailsData!.cartOrder!.createdAt!)
-                        : " No date available")
-            ),
-            content: SizedBox.shrink(),
-            isActive: _statusValue >= 0,
-          ),
-
-          Step(
-            title: Text("Packing"),
-            subtitle: Text("Order Packing is on progress.\nDate : "+((orderDetailsData!.cartOrder!.packedAt != null && orderDetailsData!.cartOrder!.packedAt!.isNotEmpty)
-                ? getFormatedDate(orderDetailsData!.cartOrder!.packedAt!)
-                : " No date available")),
-            content: SizedBox.shrink(),
-            isActive: _statusValue >= 1,
-          ),
-          Step(
-            title: Text("Out for delivery"),
-            subtitle: Text("Out for delivery.You will get your product soon.\nDate : "+((orderDetailsData!.cartOrder!.shippedAt != null && orderDetailsData!.cartOrder!.shippedAt!.isNotEmpty)
-                ? getFormatedDate(orderDetailsData!.cartOrder!.shippedAt!)
-                : " No date available")),
-            content: SizedBox.shrink(),
-            isActive: _statusValue >= 2,
-          ),
-
-          Step(
-            title: Text("Delivered"),
-            subtitle: Text("Order Delivered successfully.\nDate : "+((orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency != null && orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency!.isNotEmpty)
-                ? getFormatedDate(orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency!)
-                : " No date available")),
-            content: SizedBox.shrink(),
-            isActive: _statusValue >= 3,
-          ),
-          Step(
-            title: Text("Returned"),
-            subtitle: Text("Order returned successfully. Your return request accepted" + (orderDetailsData!.cartReturnRequests!.refundStatus.toString().compareTo("1")==0 ? "\n Refunded on "+((orderDetailsData!.cartReturnRequests!.refundedDate!=null)? getFormatedDate(orderDetailsData!.cartReturnRequests!.refundedDate.toString()) : "")  : "\nNot Refunded")
-
-
-              ,style: TextStyle(color: Colors.redAccent),),
-            content: SizedBox.shrink(),
-            isActive: _statusValue == 4,
-          ),
-
-
-
-        ],
-      );
-    }
-    else if(_statusValue==5){
-
+        });
+      }
+   else if(orderDetailsData.paymentDetails?.paymentStatus?.compareTo("0")==0  )
+    {
       w=Stepper(steps: [
         Step(
-          title: Text("Order Created"),
-          subtitle: Text("You have created an order.\n Date : "+((orderDetailsData!.cartOrder!.createdAt != null && orderDetailsData!.cartOrder!.createdAt!.isNotEmpty)
-              ? getFormatedDate(orderDetailsData!.cartOrder!.createdAt!)
-              : " No date available")),
+          title: Text("Order Initiated"),
+          subtitle: Text("You have initiated to create an order"),
           content: SizedBox.shrink(),
-          isActive: _statusValue >= 0,
+          isActive: true,
         ),
         Step(
-          title: Text("Cancelled"),
-          subtitle: Text("Out for delivery.You will get your product soon. \n Date : "+((orderDetailsData!.cartOrder!.cancelledAt != null && orderDetailsData!.cartOrder!.cancelledAt!.isNotEmpty)
-              ? getFormatedDate(orderDetailsData!.cartOrder!.cancelledAt!)
-              : " No date available")),
+          title: Text("Payment Failed"),
+          subtitle: Text("Your transaction failed",style: TextStyle(color: Colors.redAccent),),
           content: SizedBox.shrink(),
-          isActive: _statusValue >= 5,
+          isActive: true,
         )
       ], controlsBuilder: (context,controller){
         return const SizedBox.shrink();
       });
     }
+
+   else {
+
+     if(_statusValue==0 || _statusValue==1 || _statusValue==2 || _statusValue==3)
+    {
+    w= (orderDetailsData.cartReturnRequests!=null&&!orderDetailsData.cartReturnRequests!.id.toString().isEmpty) ? Stepper(
+    controlsBuilder: (context,controller){
+    return const SizedBox.shrink();
+    },
+    currentStep: _statusValue,
+    steps: [
+    Step(
+    title: Text("Order Created"),
+    subtitle: Text(
+    "You have created an order. \nDate :" +
+    ((orderDetailsData!.cartOrder!.createdAt != null && orderDetailsData!.cartOrder!.createdAt!.isNotEmpty)
+    ? getFormatedDate(orderDetailsData!.cartOrder!.createdAt!)
+        : " No date available")
+    ),
+    content: SizedBox.shrink(),
+    isActive: _statusValue >= 0,
+    ),
+
+    Step(
+    title: Text("Packing"),
+    subtitle: Text("Order Packing is on progress.\nDate : "+((orderDetailsData!.cartOrder!.packedAt != null && orderDetailsData!.cartOrder!.packedAt!.isNotEmpty)
+    ? getFormatedDate(orderDetailsData!.cartOrder!.packedAt!)
+        : " No date available")),
+    content: SizedBox.shrink(),
+    isActive: _statusValue >= 1,
+    ),
+    Step(
+    title: Text("Out for delivery"),
+    subtitle: Text("Out for delivery.You will get your product soon.\nDate : "+((orderDetailsData!.cartOrder!.shippedAt != null && orderDetailsData!.cartOrder!.shippedAt!.isNotEmpty)
+    ? getFormatedDate(orderDetailsData!.cartOrder!.shippedAt!)
+        : " No date available")),
+    content: SizedBox.shrink(),
+    isActive: _statusValue >= 2,
+    ),
+
+    Step(
+    title: Text("Delivered"),
+    subtitle: Text("Order Delivered successfully.\nDate : "+((orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency != null && orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency!.isNotEmpty)
+    ? getFormatedDate(orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency!)
+        : " No date available")),
+    content: SizedBox.shrink(),
+    isActive: _statusValue >= 3,
+    ),
+
+
+    Step(
+    title:(orderDetailsData!.cartReturnRequests!.status.toString().compareTo("1")==0)?
+    Text("Return request accepted") :
+    (orderDetailsData!.cartReturnRequests!.status.toString().compareTo("2")==0 ?
+    Text("Return request Rejected") :
+    Text("Return request submitted on "+getFormatedDate(orderDetailsData!.cartReturnRequests!.createdAt.toString()))) ,
+    subtitle: (orderDetailsData!.cartReturnRequests!.status.toString().compareTo("1")==0)?
+    Padding(padding: EdgeInsets.all(ResponsiveInfo.isMobile(context)?5:10),
+
+    child:Text("Your return request accepted" + (orderDetailsData!.cartReturnRequests!.refundStatus.toString().compareTo("1")==0 ? "\n Refunded on "+((orderDetailsData!.cartReturnRequests!.refundedDate!=null)? getFormatedDate(orderDetailsData!.cartReturnRequests!.refundedDate.toString()) : "") : "\nNot Refunded")
+
+
+    ,style: TextStyle(color: Colors.green),) ,
+
+    ) : Container(),
+    content: SizedBox.shrink(),
+    isActive: _statusValue >= 3,
+    ) ,
+
+
+    ],
+    )
+        :Stepper(
+    controlsBuilder: (context,controller){
+    return const SizedBox.shrink();
+    },
+    currentStep: _statusValue,
+    steps: [
+    Step(
+    title: Text("Order Created"),
+    subtitle: Text(
+    "You have created an order. \nDate :" +
+    ((orderDetailsData!.cartOrder!.createdAt != null && orderDetailsData!.cartOrder!.createdAt!.isNotEmpty)
+    ? getFormatedDate(orderDetailsData!.cartOrder!.createdAt!)
+        : " No date available")
+    ),
+    content: SizedBox.shrink(),
+    isActive: _statusValue >= 0,
+    ),
+
+    Step(
+    title: Text("Packing"),
+    subtitle: Text("Order Packing is on progress.\nDate : "+((orderDetailsData!.cartOrder!.packedAt != null && orderDetailsData!.cartOrder!.packedAt!.isNotEmpty)
+    ? getFormatedDate(orderDetailsData!.cartOrder!.packedAt!)
+        : " No date available")),
+    content: SizedBox.shrink(),
+    isActive: _statusValue >= 1,
+    ),
+    Step(
+    title: Text("Out for delivery"),
+    subtitle: Text("Out for delivery.You will get your product soon.\nDate : "+((orderDetailsData!.cartOrder!.shippedAt != null && orderDetailsData!.cartOrder!.shippedAt!.isNotEmpty)
+    ? getFormatedDate(orderDetailsData!.cartOrder!.shippedAt!)
+        : " No date available")),
+    content: SizedBox.shrink(),
+    isActive: _statusValue >= 2,
+    ),
+
+    Step(
+    title: Text("Delivered"),
+    subtitle: Text("Order Delivered successfully.\nDate : "+((orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency != null && orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency!.isNotEmpty)
+    ? getFormatedDate(orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency!)
+        : " No date available")),
+    content: SizedBox.shrink(),
+    isActive: _statusValue >= 3,
+    ),
+
+
+    ],
+    );
+    }
+    else if(_statusValue==4){
+  w= Stepper(
+  controlsBuilder: (context,controller){
+  return const SizedBox.shrink();
+  },
+  currentStep: _statusValue,
+  steps: [
+  Step(
+  title: Text("Order Created"),
+  subtitle: Text(
+  "You have created an order. \nDate :" +
+  ((orderDetailsData!.cartOrder!.createdAt != null && orderDetailsData!.cartOrder!.createdAt!.isNotEmpty)
+  ? getFormatedDate(orderDetailsData!.cartOrder!.createdAt!)
+      : " No date available")
+  ),
+  content: SizedBox.shrink(),
+  isActive: _statusValue >= 0,
+  ),
+
+  Step(
+  title: Text("Packing"),
+  subtitle: Text("Order Packing is on progress.\nDate : "+((orderDetailsData!.cartOrder!.packedAt != null && orderDetailsData!.cartOrder!.packedAt!.isNotEmpty)
+  ? getFormatedDate(orderDetailsData!.cartOrder!.packedAt!)
+      : " No date available")),
+  content: SizedBox.shrink(),
+  isActive: _statusValue >= 1,
+  ),
+  Step(
+  title: Text("Out for delivery"),
+  subtitle: Text("Out for delivery.You will get your product soon.\nDate : "+((orderDetailsData!.cartOrder!.shippedAt != null && orderDetailsData!.cartOrder!.shippedAt!.isNotEmpty)
+  ? getFormatedDate(orderDetailsData!.cartOrder!.shippedAt!)
+      : " No date available")),
+  content: SizedBox.shrink(),
+  isActive: _statusValue >= 2,
+  ),
+
+  Step(
+  title: Text("Delivered"),
+  subtitle: Text("Order Delivered successfully.\nDate : "+((orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency != null && orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency!.isNotEmpty)
+  ? getFormatedDate(orderDetailsData!.cartOrder!.deliveryRecvedDateFromAgency!)
+      : " No date available")),
+  content: SizedBox.shrink(),
+  isActive: _statusValue >= 3,
+  ),
+  Step(
+  title: Text("Returned"),
+  subtitle: Text("Order returned successfully. Your return request accepted" + (orderDetailsData!.cartReturnRequests!.refundStatus.toString().compareTo("1")==0 ? "\n Refunded on "+((orderDetailsData!.cartReturnRequests!.refundedDate!=null)? getFormatedDate(orderDetailsData!.cartReturnRequests!.refundedDate.toString()) : "") : "\nNot Refunded")
+
+
+  ,style: TextStyle(color: Colors.redAccent),),
+  content: SizedBox.shrink(),
+  isActive: _statusValue == 4,
+  ),
+
+
+  ],
+  );
+  }
+    else if(_statusValue==5){
+
+  w=Stepper(steps: [
+  Step(
+  title: Text("Order Created"),
+  subtitle: Text("You have created an order.\n Date : "+((orderDetailsData!.cartOrder!.createdAt != null && orderDetailsData!.cartOrder!.createdAt!.isNotEmpty)
+  ? getFormatedDate(orderDetailsData!.cartOrder!.createdAt!)
+      : " No date available")),
+  content: SizedBox.shrink(),
+  isActive: _statusValue >= 0,
+  ),
+  Step(
+  title: Text("Cancelled"),
+  subtitle: Text("Order cancelled on  \n Date : "+((orderDetailsData!.cartOrder!.cancelledAt != null && orderDetailsData!.cartOrder!.cancelledAt!.isNotEmpty)
+  ? getFormatedDate(orderDetailsData!.cartOrder!.cancelledAt!)
+      : " No date available")),
+  content: SizedBox.shrink(),
+  isActive: _statusValue >= 5,
+  )
+  ], controlsBuilder: (context,controller){
+  return const SizedBox.shrink();
+  });
+  }
+  }
     return w;
   }
 
